a9220ec496279e5811e047cd7026aa8f6af98bda
Astalyne <khkh.31@hotmail.com>
2019-03-18 15:14:06 +0300

    Modified Detail Activity Layout, Added populateUI Function and Jsonify Util

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,33 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+
+
+        TextView origin = findViewById(R.id.origin_tv);
+        TextView alsoKnownAs = findViewById(R.id.also_known_tv);
+        TextView description = findViewById(R.id.description_tv);
+        TextView ingredients = findViewById(R.id.ingredients_tv);
+
+        String originstring = sandwich.getPlaceOfOrigin();
+        origin.setText(originstring);
+        List<String> alsoKnownAsList = sandwich.getAlsoKnownAs();
+        StringBuilder names = new StringBuilder();
+
+        for(String name :alsoKnownAsList)
+            names.append(name).append(" , ");
+        alsoKnownAs.setText(names);
+        description.setText(sandwich.getDescription());
+
+        List<String> ings = sandwich.getIngredients();
+
+        StringBuilder in = new StringBuilder();
+
+        for (String ing : ings)
+            in.append(ing).append(" ,");
+
+        ingredients.setText(in);
+
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,65 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
+    public static final String JSON_NAME = "name";
+    public static final String JSON_MAIN_NAME = "mainName";
+    public static final String JSON_ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String JSON_PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String JSON_DESCRIPTION = "description";
+    public static final String JSON_IMAGE = "image";
+    public static final String JSON_INGREDIENTS = "ingredients";
+
+
+
+
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sw = new Sandwich();
+
+
+
+        try{
+
+            JSONObject jobject = new JSONObject(json);
+    JSONObject object = jobject.getJSONObject(JSON_NAME);
+            JSONArray alsoKnownAsJsonArray = object.getJSONArray(JSON_ALSO_KNOWN_AS);
+
+            List<String> alsoKnownAsArray = new ArrayList<>();
+
+            for (int i = 0; i<alsoKnownAsJsonArray.length();i++){
+
+                alsoKnownAsArray.add(alsoKnownAsJsonArray.getString(i));
+
+            }
+            sw.setAlsoKnownAs(alsoKnownAsArray);
+
+            sw.setDescription(jobject.getString(JSON_DESCRIPTION));
+            sw.setImage(jobject.getString(JSON_IMAGE));
+            sw.setPlaceOfOrigin(jobject.getString(JSON_PLACE_OF_ORIGIN));
+
+            List<String> ing = new ArrayList<>();
+            JSONArray ingArray = jobject.getJSONArray(JSON_INGREDIENTS);
+            for (int i =0; i<ingArray.length();i++){
+                ing.add(ingArray.getString(i));
+            }
+            sw.setIngredients(ing);
+
+
+
+
+
+
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+        return sw;
     }
 }
